8727b70b3a06dbe4838f0cb9a663b14b5d66c0d3
jyz <ndlgyb@qq.com>
2013-11-22 11:49:10 +0800

    生成短PK

---
+++ src/main/java/com/eighthinfo/sls/utils/UUIDGen.java
@@ -0,0 +1,36 @@
+package com.eighthinfo.sls.utils;
+
+
+
+import org.apache.commons.lang.time.StopWatch;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dam
+ * Date: 13-11-22
+ * Time: 上午11:28
+ * To change this template use File | Settings | File Templates.
+ */
+public class UUIDGen {
+
+    static final String[] strs = new String[]{"a", "b", "c", "d", "e", "f",
+            "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+            "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5",
+            "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I",
+            "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
+            "W", "X", "Y", "Z"};
+    public static String genShortPK(){
+        String uuid = UUID.randomUUID().toString().replace("-","");
+        StringBuffer sb = new StringBuffer("");
+        for(int i = 0;i < 8; i ++){
+            String str = uuid.substring(i *4 ,i*4+4);
+            int x = Integer.parseInt(str,16);
+            sb.append(strs[x % 0x3E]);
+        }
+        return sb.toString();
+    }
+}

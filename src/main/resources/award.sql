bb3824067e12ebecee8108a055eeb77c3b64d2be
jyz <ndlgyb@qq.com>
2013-12-16 15:51:21 +0800

    添加中奖概率字段

--- src/main/resources/award.sql
+++ src/main/resources/award.sql
@@ -1,11 +1,12 @@
 CREATE TABLE `t_award` (
   `award_id` varchar(50) NOT NULL,
-  `award_name` varchar(100) default NULL,
-  `bg_href` varchar(200) default NULL COMMENT '房间图片地址',
+  `award_name` varchar(100) DEFAULT NULL,
+  `bg_href` varchar(200) DEFAULT NULL COMMENT '房间图片地址',
   `remain` int(8) NOT NULL COMMENT '剩下的奖品数',
   `total` int(8) NOT NULL COMMENT '总奖品数',
-  `detail_href` varchar(200) default NULL COMMENT '详情介绍链接',
-  `description` varchar(500) default NULL,
-  `price` int(8) default NULL COMMENT '价值，元，省略角分单位',
-  PRIMARY KEY  (`award_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+  `detail_href` varchar(200) DEFAULT NULL COMMENT '详情介绍链接',
+  `description` varchar(500) DEFAULT NULL,
+  `price` int(8) DEFAULT NULL COMMENT '价值，元，省略角分单位',
+  `rate_of_win` double DEFAULT NULL COMMENT '中奖概率',
+  PRIMARY KEY (`award_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;

2aee7325fabb410c09dc0e1930c99854f4f22484
jyz <ndlgyb@qq.com>
2013-12-22 15:47:19 +0800

    添加玩家级别初始化数据

--- src/main/resources/t_player_level_setting.sql
+++ src/main/resources/t_player_level_setting.sql
@@ -4,3 +4,8 @@ CREATE TABLE `t_player_level_setting` (
   `need_exp` int(11) DEFAULT NULL COMMENT '需要的经验值',
   PRIMARY KEY (`level_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+INSERT INTO `t_player_level_setting` (`level_id`, `level_title`, `need_exp`) VALUES ('1', '进士', 0);
+INSERT INTO `t_player_level_setting` (`level_id`, `level_title`, `need_exp`) VALUES ('2', '举人', 300);
+INSERT INTO `t_player_level_setting` (`level_id`, `level_title`, `need_exp`) VALUES ('3', '秀才', 700);
+INSERT INTO `t_player_level_setting` (`level_id`, `level_title`, `need_exp`) VALUES ('4', '状元', 1500);
